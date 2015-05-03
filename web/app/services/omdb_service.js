b868d10c995d5ca8addd8a6d0a69c7baf990a7de
broileri <jenna.lindh@luukku.com>
2015-05-03 21:42:53 +0300

    req

--- web/app/services/omdb_service.js
+++ web/app/services/omdb_service.js
@@ -1,6 +1,15 @@
 MovieApp.service('OMDbService', function ($http) {
     this.findMovie = function (name, year) {
-        return $http.get('//www.omdbapi.com', {params: {s: name, y: year}});
+        if (!year) {
+            year = '';
+        }
+        var req = {
+            method: 'GET',
+            url: 'http://www.omdbapi.com/?s=' +name+ '&y=' + year
+        };
+        
+        return $http(req);
+        //return $http.get('//www.omdbapi.com', {params: {s: name, y: year}});
     };
 });
 

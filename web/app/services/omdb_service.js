f87146ad23f1ef68673c7c8901f24251bb753296
Broileri <jelindh@cs.helsinki.fi>
2016-08-28 04:38:17 +0300

    links

--- web/app/services/omdb_service.js
+++ web/app/services/omdb_service.js
@@ -5,7 +5,7 @@ MovieApp.service('OMDbService', function ($http) {
         }
         var req = {
             method: 'GET',
-            url: 'https://www.omdbapi.com/?s=' +name+ '&y=' + year
+            url: '//www.omdbapi.com/?s=' +name+ '&y=' + year
         };
         
         return $http(req);
